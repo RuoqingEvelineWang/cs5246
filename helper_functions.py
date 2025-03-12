3ee520e9f5d45b99500618780227e7250fd2b48a
Eka Buyung Lienadi <ekabuyung@gmail.com>
2025-03-12 16:14:59 +0800

    notebook for paper summarization task

--- helper_functions.py
+++ helper_functions.py
@@ -5,7 +5,12 @@ from io import BytesIO
 
 from collections import Counter
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 import string
+
+import re
+
 
 def pad_arxiv_id(arxiv_id):
     parts = str(arxiv_id).split(".")
@@ -33,5 +38,52 @@ def extract_pdf_text(arxiv_id):
     except Exception as e:
         return f"Error extracting text: {e}"
     
+#tokenization and text cleaning
+def preprocess(text):
+    lemmatizer = WordNetLemmatizer()
+    stop_words = set(stopwords.words('english'))
+
+    # Lowercase and remove URLs/special characters
+    text = text.lower()
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    #text = re.sub(r'[^a-zA-Z\s]', '', text).strip()
     
-    #
+    #Removing Extra Spaces
+    text = re.sub(r'\s+', ' ', text).strip()
+    
+    #Joining wordssplit by -
+    text = re.sub(r'(\w+)-\s(\w+)', '\1\2', text)
+    
+    #Remove citations and references
+    text = re.sub(r'\[\d+\]', '', text)  # Removes [12]
+    text = re.sub(r'\(.*?et al\.,\s*\d{4}\)', '', text)  # Removes (Smith et al., 2020)
+    ref = "references"
+    text = text.split(ref)[0].strip() if ref in text else text
+    
+    #Remove content before introduction
+    intro = "introduction"
+    text = text.split(intro)[1].strip() if intro in text else text
+    
+    # Tokenize and lemmatize
+    tokens = word_tokenize(text)
+    tokens = [word for word in tokens if word not in stop_words]
+    
+    return ' '.join(tokens)
+
+# Join sentences while keeping within the 1024-token limit
+def chunk_text(sentences, tokenizer, max_tokens=1024):
+    chunks = []
+    current_chunk = []
+
+    for sentence in sentences:
+        tokenized_sentence = tokenizer.tokenize(sentence)
+        if len(tokenized_sentence) + sum(len(tokenizer.tokenize(s)) for s in current_chunk) < max_tokens:
+            current_chunk.append(sentence)
+        else:
+            chunks.append(" ".join(current_chunk))
+            current_chunk = [sentence]
+
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+
+    return chunks
